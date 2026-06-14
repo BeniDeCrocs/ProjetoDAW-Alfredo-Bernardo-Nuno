cd6b7b49a982d51aed7a8af5e78e9f4503ebdf68
nunooliveiraa14 <nunocoliveiraa@gmail.com>
2026-06-14 18:42:32 +0100

    Código Corrigido

--- server.py
+++ server.py
@@ -25,7 +25,7 @@ def create_app():
     app.add_url_rule("/recuperar", view_func=views.recover_page, methods=["GET", "POST"])
     app.add_url_rule("/logout", view_func=views.logout_page)
     
-    # Rotas do Jogo (APENAS UMA VEZ CADA!)
+    # Rotas do Jogo
     app.add_url_rule("/salvar-progresso", view_func=views.salvar_progresso, methods=["POST"])
     app.add_url_rule("/comprar-estrutura", view_func=views.comprar_estrutura, methods=["POST"])
     app.add_url_rule("/vender-estrutura", view_func=views.vender_estrutura, methods=["POST"])
